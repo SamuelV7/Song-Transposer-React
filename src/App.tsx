cb70dc2c72b76216338bb3621aa4e4f05ac80615
Samuel Varghese <gamingsam77@gmail.com>
2022-05-09 00:23:46 +0100

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Page from './Layout/Page';
+
+function App() {
+  return (
+    <div className="App">
+      <Page />
+      {/* <header className="App-header">
+        <h2 className="text-3xl font-bold">Soli Deo Gloria</h2>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
