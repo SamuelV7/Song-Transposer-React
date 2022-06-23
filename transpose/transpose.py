37d2f861e37350219ff66d0a60708f4f69ab1bc0
Samuel Varghese <gamingsam77@gmail.com>
2022-06-23 18:40:09 +0100

    Can now display text in multiline instead of one array, need to work on making it look better and make sure space between chords are not lost.

--- transpose/transpose.py
+++ transpose/transpose.py
@@ -2,6 +2,7 @@ from cgi import print_arguments
 import pdfplumber
 from pychord import Chord
 import argparse
+import json
 
 def get_pdf(path):
     pdf = pdfplumber.open(path)
@@ -45,8 +46,7 @@ def pdf_transpose(path: str, transpose_int: int):
     pdf_pages = get_pdf(path)
     the_texts = extract_pages_text(pdf_pages)
     newList = transpose_text(the_texts[0], transpose_int)
-    return newList
-
+    return json.dumps(newList)
 
 if __name__ == "__main__":
     # arguments, file to transpose and how much steps to transpose
