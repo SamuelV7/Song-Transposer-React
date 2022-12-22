173ee286eacd890408bf1dc0a975f124f069ee22
Samuel Varghese <gamingsam77@gmail.com>
2022-12-22 15:45:56 +0000

    pdf added

--- gll-music/src/routes/+page.svelte
+++ gll-music/src/routes/+page.svelte
@@ -9,9 +9,11 @@
 	</button>
 </div>
 
-<Container fluid='true'>
+<Container fluid='true' class="songview">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </Container>
 
-<style lang="scss"></style>
+<style lang="scss">
+	
+</style>
