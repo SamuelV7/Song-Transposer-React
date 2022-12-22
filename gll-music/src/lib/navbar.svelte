48bd3defa1e495882147fb52bee9cbb4443a42df
Samuel Varghese <gamingsam77@gmail.com>
2022-12-22 15:26:04 +0000

    Moving to svelte

---
+++ gll-music/src/lib/navbar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Navbar, NavItem, Nav, NavLink, NavbarBrand } from 'sveltestrap/src'
+</script>
+
+<Navbar color='light' light expand='md'>
+    <NavbarBrand href="/">GLL Music</NavbarBrand>
+    <Nav>
+        <NavItem>
+            <NavLink href="/songview">Songs List</NavLink>
+        </NavItem>
+    </Nav>
+</Navbar>
---
+++ gll-music/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import {Container} from 'sveltestrap/src'
+</script>
+
+
+<div>
+	<button class="btn btn-primary">
+	Button	
+	</button>
+</div>
+
+<Container fluid='true'>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Container>
+
+<style lang="scss"></style>
